a2df289a13e4819b1bb26bac7e2ab88d57170e44
username <wecp-dummy@gmail.com>
2025-01-24 12:19:24 +0000

    wecp initial commit

--- frontend/src/app/ipl/components/cricketercreate/cricketercreate.component.ts
+++ frontend/src/app/ipl/components/cricketercreate/cricketercreate.component.ts
@@ -1,4 +1,29 @@
 
-export class CricketerCreateComponent {
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+    selector:'app-crickaterCreateComponent',
+    templateUrl:'./cricketercreate.component.html',
+    styleUrls:['./cricketercreate.component.scss']
+})
+export class CricketerCreateComponent implements OnInit {
+    cricketerForm: FormGroup;
+    successMessage: string | null=null;
+    errorMessage: string | null=null;
+    
+    constructor(private fb:FormBuilder){
+    }
+
+
+    ngOnInit(): void {
+        this.cricketerForm = this.fb.group({
+            cricketerId:[null,Validators.required, Validators.minLength(1)],
+            cricketerName:['',Validators.required, Validators.minLength(2)],
+            location: ['',Validators.required],
+            ownerName:['',Validators.required,Validators.minLength(2)],
+            establishmentYear:[null,Validators.required]
+          })
+    }
 
 }
--- frontend/src/app/ipl/ipl.module.ts
+++ frontend/src/app/ipl/ipl.module.ts
@@ -4,9 +4,14 @@ import { CommonModule } from "@angular/common";
 import { IplRoutingModule } from "./ipl-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamCreateComponent,
+    CricketerCreateComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
