dc2b962434a1566881cfe5480eebd4a42f1af7d9
username <wecp-dummy@gmail.com>
2025-01-30 12:12:14 +0000

    wecp initial commit

--- frontend/src/app/ipl/components/cricketercreate/cricketercreate.component.ts
+++ frontend/src/app/ipl/components/cricketercreate/cricketercreate.component.ts
@@ -1,4 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Cricketer } from '../../types/Cricketer';
 
-export class CricketerCreateComponent {
+@Component({
+  selector: 'app-cricketercreate',
+  templateUrl: './cricketercreate.component.html',
+  styleUrls: ['./cricketercreate.component.scss']
+})
+export class CricketerCreateComponent implements OnInit {
+  cricketerForm!: FormGroup;
+  cricketer: Cricketer | null = null;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.cricketerForm = this.formBuilder.group({
+      cricketerId: [null, Validators.required],
+      teamId: [null, Validators.required],
+      cricketerName: ['', Validators.required],
+      age: [null, [Validators.required, Validators.min(18)]],
+      nationality: ['', Validators.required],
+      experience: [null, Validators.required],
+      role: ['', Validators.required],
+      totalRuns: [null, Validators.min(0)],
+      totalWickets: [null, Validators.min(0)],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.cricketerForm.valid) {
+      this.cricketer = this.cricketerForm.value;
+      this.successMessage = 'Cricketer created successfully!';
+      console.log(this.cricketer);
+      this.resetForm();
+      this.errorMessage = null;
+    } else {
+      this.errorMessage = 'Please fill out all required fields correctly.';
+      this.successMessage = null;
+    }
+  }
+
+  resetForm(): void {
+    this.cricketerForm.reset();
+  }
 }
